477ceb0eb80b37988dd7abfebc5d085d59b215d2
Daniel Ayala <dmayala001@gmail.com>
2016-04-14 12:36:25 -0700

    Add authentication controller

---
+++ server/controllers/authentication.js
@@ -0,0 +1,9 @@
+exports.signup = function (req,res,next) {
+  // see if a user with the given email exists
+  
+  // if a user with email does exist, return an error
+  
+  // If a user with email does not exist, create and save user record
+
+  // Respond to request indicating the user was created
+}
